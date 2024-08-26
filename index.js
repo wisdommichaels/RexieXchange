2469d2579a8848d108f438674065ab91da810ce3
wisdom michael <ijarwisdom@gmail.com>
2024-08-26 18:45:56 +0100

    added js

--- index.js
+++ index.js
@@ -51,3 +51,26 @@
     const targetSlide = slides[currentSlide];
     moveToSlide(track, currentSlide, targetSlide);
   }, 3000);
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const cards = document.querySelectorAll('.scroll-card');
+
+    const revealOnScroll = () => {
+        const triggerBottom = window.innerHeight * 0.85;
+
+        cards.forEach(card => {
+            const cardTop = card.getBoundingClientRect().top;
+
+            if (cardTop < triggerBottom) {
+                card.classList.add('show');
+            } else {
+                card.classList.remove('show');
+            }
+        });
+    };
+
+    // Trigger the function on page load and scroll
+    revealOnScroll();
+    window.addEventListener('scroll', revealOnScroll);
+});
