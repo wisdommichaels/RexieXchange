6b238e8c9b9524b1875f56df44373c268e9fcc6a
wisdom michael <ijarwisdom@gmail.com>
2024-09-06 22:49:40 +0100

    added login page

--- index.js
+++ index.js
@@ -74,3 +74,17 @@
     revealOnScroll();
     window.addEventListener('scroll', revealOnScroll);
 });
+
+
+// js for login/signup page.
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
+registerBtn.addEventListener('click', ()=>{
+    container.classList.add('active');
+});
+
+loginBtn.addEventListener('click', ()=>{
+    container.classList.remove('active');
+});
