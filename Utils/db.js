383bd71f4ce1dfa25ae00a9d9ad0a781c29edaff
wisdom michael <ijarwisdom@gmail.com>
2024-09-08 02:07:33 +0100

    added npm

---
+++ Utils/db.js
@@ -0,0 +1,14 @@
+import  mongoose from "mongoose";
+
+const connectdb = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            dbname:"finnal-project"
+        });
+            console.log("Connected to MongoDB", conn.connection.host);
+    } catch (error) {
+        console.error("Failed to connect to MongoDB", error);
+    }
+    
+}
+export default connectdb;
