18d3c2bdc1e3563d3a8f399eb15f2aabf8306d9a
wisdom michael <ijarwisdom@gmail.com>
2024-11-11 15:08:24 +0100

    code optimized

--- Server/Server.js
+++ Server/Server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors(
     {
-        origin: ["http://localhost:5173","http://localhost:5174","http://localhost:5175"],
+        origin: ["http://localhost:5173","http://localhost:5174","http://localhost:5175","https://gift-hub-35.vercel.app/"],
         credentials: true,
     }
 ))
