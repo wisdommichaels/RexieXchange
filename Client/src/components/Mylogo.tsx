85475600dfe6b195414d0d2bd4d07ca684e74fa7
wisdom michael <ijarwisdom@gmail.com>
2024-12-16 08:24:23 +0100

    code optimized26

--- Client/src/components/Mylogo.tsx
+++ Client/src/components/Mylogo.tsx
@@ -7,7 +7,7 @@ const Mylogo = () => {
         <img className="sm:w-[80px] sm:h-[80px] w-[40px] h-[40px] sm:pb-9" src="https://res.cloudinary.com/duwfbyhyq/image/upload/v1734048565/logo_l22er0.webp" alt="" />
         <div className="flex-col justify-center items-center leading-3 sm:leading-[1] sm:pb-9 pt-[7px] sm:p-0">
         <h1 className="font-bold sm:text-[20px] text-[14] sm:text-[#161D6F] text-white">RexieXchange</h1>
-        <p className="sm:text-[10px] text-[5.8px] sm:text-[#161D6F] text-white">GiftCard Xchange Made Easy</p>
+        <p className="sm:text-[10px] text-[6px] sm:text-[#161D6F] text-white">GiftCard Xchange Made Easy</p>
         </div>
       </Link>
 
